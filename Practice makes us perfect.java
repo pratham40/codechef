a4472be64bb9e61503e2d258e3fe3f2e9c7cecf2
Pratham <122166099+pratham40@users.noreply.github.com>
2023-09-17 20:06:15 +0530

    Create Practice makes us perfect.java

---
+++ Practice makes us perfect.java
@@ -0,0 +1,26 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int[] arr=new int[4];
+		for(int i=0;i<4;i++){
+		    arr[i]=sc.nextInt();
+		}
+		int count=0;
+		for (int j:arr ){
+		    if(j>=10){
+		        count++;
+		    }
+		}
+		System.out.print(count);
+	}
+}
